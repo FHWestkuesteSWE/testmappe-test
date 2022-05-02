3f438985042d36a8c5ffff9fa68dce422604cf49
kiranrajk24x7 <kiranrajkrishnakumar@gmail.com>
2022-05-02 14:00:44 +0200

    test

--- TestProjekt/TestProjekt.cpp
+++ TestProjekt/TestProjekt.cpp
@@ -8,7 +8,7 @@ int main()
     std::cout << "Hello World!\n";
     //Return Test
     return 0;
-    //Test
+   // test2hbbdk
 }
 
 // Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
